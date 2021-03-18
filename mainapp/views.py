e44087e50ca237de0cfd061f16debf91336aa753
Alex_Tarasov <taralser@mail.ru>
2021-03-18 23:56:43 +0300

    Lesson 4

---
+++ mainapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from mainapp.models import Product, ProductCategory
+
+
+def main(request):
+    products = Product.objects.all()[:4]
+    content = {'title': 'Главная', 'products': products}
+    return render(request, 'mainapp/index.html', content)
+
+
+def products(request, pk=None):
+    links_menu = ProductCategory.objects.all()
+    content = {
+        'title': 'Продукты',
+        'links_menu': links_menu
+    }
+    return render(request, 'mainapp/products.html', content)
+
+
+def contact(request):
+    content = {
+        'title': 'Контакты'
+    }
+    return render(request, 'mainapp/contact.html', content)
